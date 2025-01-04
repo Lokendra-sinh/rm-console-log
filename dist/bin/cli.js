8f718908d02a707b37e131165db65d499b0b88b4
Lokendra <anizen242@gmail.com>
2025-01-04 11:13:37 +0530

    Remove dist and update .gitignore

--- dist/bin/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../src/index");
-(0, index_1.processFiles)().catch((error) => {
-    console.error('Fatal error:', error instanceof Error ? error.message : String(error));
-    process.exit(1);
-});
-//# sourceMappingURL=cli.js.map
